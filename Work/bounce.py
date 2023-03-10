12799a2657cbc4bcfac36d752918fcc7055f433b
Jozef M <67822247+resolux66@users.noreply.github.com>
2023-03-10 09:31:10 +0000

    Finished exercises on bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,7 @@
 # bounce.py
 #
 # Exercise 1.5
+hight = 100
+for i in range(1, 11):
+    hight = hight / 5 * 3
+    print(i, round(hight, 4))
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,22 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+
+while principal > 0:
+    months += 1
+    
+    principal = principal * (1 + rate / 12) - payment
+    total_paid = total_paid + payment
+    if months < 13:
+        total_paid += 1000
+        principal -= 1000
+    print(total_paid)
+    
+
+print ('Total paid', round(total_paid))
+print(months)
