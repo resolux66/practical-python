0da5eafa2e6a89aad86b63fddbbdd329b2efa001
resolux66 <67822247+resolux66@users.noreply.github.com>
2023-03-17 19:24:03 +0000

    Update pcost.py
    solution for exercise

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+with open('Work/Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    total_cost = 0
+    for line in f:
+        row = line.split(',')
+        cost = int(row[1]) * float(row[2])
+        total_cost += cost
+    print(f'{total_cost:.2f}')
